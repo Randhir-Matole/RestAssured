814ad76fcba58542e11aab8358237dc70b798f14
randhir <randhirmatole@gmail.com>
2023-04-06 21:39:59 +0530

    First Commit

---
+++ src/postDriverClass.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+import postTestCases.postTc1;
+import postTestCases.postTc2;
+import postTestCases.postTc3;
+
+public class postDriverClass {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		postTc1.post_Executer_Tc1();
+		postTc2.post_Executer_Tc2();
+		postTc3.post_Executer_Tc3();
+	}
+
+}
---
+++ src/putDriverClass.java
@@ -0,0 +1,15 @@
+import putTestCases.putTc1;
+import putTestCases.putTc2;
+import putTestCases.putTc3;
+
+public class putDriverClass {
+
+	public static void main(String[] args) {
+		
+		putTc1.testResponse();
+		putTc2.testResponse();
+		putTc3.testResponse();
+
+	}
+
+}
